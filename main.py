96b162179d2b7d9dc7dba219f2abc83a5cbc8d4d
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2024-01-31 14:24:45 -0500

    added wide>long formatter for all predictions data set, updated export locations, added some printouts

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def main():
     """    
     # Set file paths
     raw_bank_csv_filepath = os.path.join('.', 'data', 'bank.csv')
-    model_predictions_bank_csv_filepath = os.path.join('.', 'data', 'bank_model predictions.csv')
-    model_coefficients_bank_csv_filepath = os.path.join('.', 'data', 'bank_model coefficients.csv')
-    
+    model_testsplit_predictions_csv_filepath = os.path.join('.', 'data', 'model_output', 'bank_model testing split predictions.csv')
+    model_coefficients_csv_filepath = os.path.join('.', 'data', 'model_output', 'bank_model coefficients.csv')
+    model_all_predictions_csv_filepath = os.path.join('.', 'data', 'model_output', 'bank_predictions on all data.csv')
+
     # Create a data loader object and load data
     data_loader = DataHandler(filepath=raw_bank_csv_filepath)
     data = data_loader.data
@@ -31,6 +32,8 @@ def main():
     # Run the data modeler (outputs x_test predictions, coefficients, odds ratios)
     data_modeler.run()
 
+    print('\n----------------------------')
+    print('EXPORT TEST SPLIT PREDICTIONS'.center(28, '-'))
     # Consolidate one-hot encoded columns
     prefixes = ['age_', 'job_', 'marital_', 'education_']
     for prefix in prefixes:
@@ -48,25 +51,55 @@ def main():
             column,
             drop_original=True
             )
-    print('\n----------------------------')
-    print('data prepared for export'.center(28, '-'))
+        
+    print('test split predictions export ready'.center(28, '-'))
     print(type(data_modeler.x_test))
     print(data_modeler.x_test.head(5))
-
-    # Export data
-    print('\n----------------------------')
-    print('EXPORT PREDICTIONS'.center(28, '-'))
     data_loader.export_data(
         data_modeler.x_test, 
-        model_predictions_bank_csv_filepath
+        model_testsplit_predictions_csv_filepath
+        )
+
+    print('\n----------------------------')
+    print('EXPORT MODEL ALL PREDICTIONS'.center(28, '-'))
+    # Consolidate one-hot encoded columns
+
+    print(data_modeler.all_processed_data_with_predictions.dtypes)
+
+    prefixes = ['age_', 'job_', 'marital_', 'education_']
+    for prefix in prefixes:
+        data_modeler.all_processed_data_with_predictions = utilities.consolidate_one_hot_columns(
+            data_modeler.all_processed_data_with_predictions, 
+            prefix, 
+            drop_original=True
+            )
+
+    # Convert binary values (0, 1) to boolean strings ('false', 'true')
+    columns = ['default_yes', 'housing_yes', 'loan_yes']
+    for column in columns:
+        data_modeler.all_processed_data_with_predictions = utilities.convert_binary_to_boolean(
+            data_modeler.all_processed_data_with_predictions, 
+            column,
+            drop_original=True
+            )
+        
+    print('all predictions export ready'.center(28, '-'))
+    print(type(data_modeler.all_processed_data_with_predictions))
+    print(data_modeler.all_processed_data_with_predictions.head(75))
+    data_loader.export_data(
+        data_modeler.all_processed_data_with_predictions, 
+        model_all_predictions_csv_filepath
         )
 
     print('\n----------------------------')
     print('EXPORT COEFFICIENTS'.center(28, '-'))
     data_loader.export_data(
         data_modeler.coeff_summary, 
-        model_coefficients_bank_csv_filepath
+        model_coefficients_csv_filepath
         )
+    
+    return data_modeler
 
 if __name__ == "__main__":
-    data_loader = main()
+    data_modeler = main()
+    print(data_modeler.all_processed_data_with_predictions.sort_values(by='deposit', ascending=True).head(75))
